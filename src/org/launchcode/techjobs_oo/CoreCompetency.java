a405e66987354167694ffdcd587e570b9fe31595
Mark <Markgarvey42@gmail.com>
2021-05-20 14:48:07 -0500

    Location Class - Added a second constructor to initialize the id and value field
    CoreCompetency Class - Added getters/setters for id and value fields
    
    PositionType Class - Added a toString method that returns the value and generated an equals/hashcode method

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs_oo;
 import java.util.Objects;
 
 public class CoreCompetency {
+
     private int id;
     private static int nextId = 1;
     private String value;
@@ -39,4 +40,16 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
